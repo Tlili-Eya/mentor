776f815c5e39d23ae0b7603af8d6f34f816bf999
amalMok <amal.mokdad07@gmail.com>
2026-02-09 22:12:09 +0100

    Fix routes et base layout après sync

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -13,7 +13,6 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use App\Repository\FeedbackRepository;
